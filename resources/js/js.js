f030bd7c493df7fa1f13a2e6c6ce9cd0e8b39a5d
Liam Telenko <mailtelenko@gmail.com>
2020-03-17 23:37:24 -0400

    Initial commit - Desktop OK

--- resources/js/js.js
+++ resources/js/js.js
@@ -1,5 +1,7 @@
+// Variable declaration
 var dark_mode_enabled = false;
 
+// When DOM loaded
 $(document).ready(function () {
 
     // Get current hour
@@ -10,81 +12,87 @@ $(document).ready(function () {
         toggle_dark_mode();
     }
 
-    // Get original height of text block
-    const pHeight = $('.header p').height();
-
-    // When header section clicked expand
-    $(".header").click(function (e) {
-        if (event.target.id != "toggle_light")
-            toggleHeader();
+    // When expandable section is clicked, expand it
+    $(".expandable").click(function (e) {
+        toggleExpand(e);
     });
 
-    // When contact button clicked
+    // When contact button clicked scroll to section
     $("#contact_button").click(function () {
+        ("width", "200px")
         $([document.documentElement, document.body]).animate({
             scrollTop: $("#contact_section").offset().top
         }, 800);
     });
 
-    // When contact button clicked
-    $("#about_button").click(function () {
-        toggleHeader()
-    });
 
     // When dark mode toggle clicked
-    $("#dark_mode_toggle").click(function (e) {
+    $("#dark_mode_toggle").click(function () {
         toggle_dark_mode();
     });
 
+    // Initialize animate on scroll
     AOS.init();
-
 });
 
-// Will display or hide the header when called
-function toggleHeader() {
-    if ($(".header #description").css('display') == 'none') {
-        //Show element
-        $(".header #description").slideDown(400);
-        $(".header .fas").css({
-            "transform": "rotate(180deg)"
-        });
-
-        // Hide extra buttons
-        $("#lower_heading_section").slideUp(300);
+// Change height of info card when clicked
+$(".resume_item").click(function () {
+    console.log($(".under_fold").css('display'));
+    // Check if clicked item is open, if so, close it
+    if ($(this).find(".under_fold").css("display") != "none") {
+        $(".under_fold").slideUp(300);
+        $("#sneaky_overlay").fadeOut(200);
+        setTimeout(function () {
+            $(".resume_item").css({"z-index": "1", "transform": "none"});
+        }, 300);
     } else {
-        //Hide element
-        $(".header #description").slideUp(400);
-        $(".header .fas").css({
-            "transform": "rotate(0deg)"
+        $("#sneaky_overlay").fadeIn(200);
+        $(this).css({
+            'overflow': 'visible',
+            "z-index": "10",
+            "transform": "scale(1.05,1.05) translateY(-10px)"
         });
-
-        // Hide extra buttons
-        $("#lower_heading_section").slideDown(300);
+        $(this).find(".under_fold").slideDown(300);
     }
+});
+
+// Display resume page
+function display_resume() {
+    $("#header_container").delay(400).animate({
+        "width": "25%",
+        "left": "0%"
+    }, 600);
+    $("#resume_section").delay(1050).fadeIn(550);
+    $("#description").fadeOut(200);
+    $("#main_nav").delay(150).fadeOut(200);
+    $("#res_nav").delay(900).fadeIn(400);
 }
 
+function hide_resume() {
+    $("#header_container").delay(650).animate({
+        "width": "50%",
+        "left": "25%"
+    }, 500);
+    $("#resume_section").fadeOut(550);
+    $("#main_nav").delay(1150).fadeIn(350);
+    $("#description").delay(1050).fadeIn(200);
+    $("#res_nav").delay(450).fadeOut(300);
+}
+//display_resume();
+
+// Will display or hide the header when called
+function toggleExpand(e) {
+    $(e.currentTarget).find(".hidable").slideToggle(300);
+    $(".header .fas").css({
+        "transform": "rotate(180deg)"
+    });
+}
+
+// Changes data-theme attribute to dark or light depending on current value
 function toggle_dark_mode() {
-    if (!dark_mode_enabled) {
-        // Reset CSS colour variables to dark theme
-        document.querySelector(":root").style.setProperty('--background_colour', "#2b2b2b");
-        document.querySelector(":root").style.setProperty('--container_colour', "#252526");
-        document.querySelector("body").style.setProperty('--text_colour', "#ffffff");
-        document.querySelector("body").style.setProperty('--text_hover', "#ffffff");
-
-        // Change tint colour (lighter)
-        $(".header p").css("background-color", "rgba(255, 255, 255, .04)");
-        $("#dark_mode_toggle").css("background-color", "rgba(255, 255, 255, .04)");
-
-    } else {
-        // Reset CSS colour variables to light theme
-        document.querySelector(":root").style.setProperty('--background_colour', "#f8f8f8");
-        document.querySelector(":root").style.setProperty('--container_colour', "#ffffff");
-        document.querySelector("body").style.setProperty('--text_colour', "#000000");
-        document.querySelector("body").style.setProperty('--text_hover', "#ffffff");
-
-        // Change tint colour (darker)
-        $(".header p").css("background-color", "rgba(0, 0, 0, .03)");
-    }
-
-    dark_mode_enabled = !dark_mode_enabled; // Set new value to var
+    // Check if current mode is dark and change attribute accordingly
+    if (document.documentElement.getAttribute('data-theme') != "dark")
+        document.documentElement.setAttribute('data-theme', 'dark');
+    else
+        document.documentElement.setAttribute('data-theme', 'light');
 }
